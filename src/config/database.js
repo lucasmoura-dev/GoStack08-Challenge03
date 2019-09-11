02115577f3f11f38832ad13b7653032936f5f456
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-11 20:46:07 -0300

    Alterado o banco de dados a ser usado

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'meetapp',
   define: {
     timestamps: true, // gera automaticamente as datas de edição e criação
     underscored: true, // define que irá utilizar o padrão de tabelas e colunas usando underlines
