40bfcf1fbc7ba2e58b95a50633243f9cfdf5625c
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-11 21:41:59 -0300

    Adicionado a dependencia do bcrypt. Adicionado a criptografia na senha do usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 import Sequelize, { Model } from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   /**
    * Vai ser chamado automaticamente pelo Sequelize
@@ -7,16 +9,30 @@ class User extends Model {
    */
   static init(sequelize) {
     // Model define os dados que serão usados do req.body, os outros serão ignorados.
+    // Não precisam ser reflexo da base de dados
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // não vai exitir no banco de dados
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    /**
+     * Vai ser executado antes de salvar/editar um usuário
+     */
+    this.addHook('beforeSave', async user => {
+      // O hash só será gerado quando estiver alterando uma senha
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this; // retorna o model que foi inicializado aqui
   }
 }
 
