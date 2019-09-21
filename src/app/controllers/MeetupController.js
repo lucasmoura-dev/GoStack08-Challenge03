3a8ba65971fa102cef61eea408905b0ac80d8b6a
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-20 23:59:22 -0300

    Adicionado a validacao de campos obrigatorios ao salvar um meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -6,8 +7,26 @@ class MeetupController {
   // }
 
   async store(req, res) {
-    const { title, date, description, location, banner_id } = req.body;
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      date: Yup.date().required(),
+      location: Yup.string().required(),
+      banner_id: Yup.number().required(),
+    });
 
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { title, date, description, location, banner_id } = req.body;
+    /*
+    Não deve ser possível cadastrar meetups com datas que já passaram.
+    O usuário também deve poder editar todos dados de meetups que ainda não aconteceram e que ele é organizador.
+
+    Crie uma rota para listar os meetups que são organizados pelo usuário logado.
+
+    O usuário deve poder cancelar meetups organizados por ele e que ainda não aconteceram. O cancelamento deve deletar o meetup da base de dados.
+    */
     const meetup = Meetup.create({
       title,
       date,
