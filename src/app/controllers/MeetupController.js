5d01b6fa84df6e0e75fee6347e4d998aecc64228
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-21 00:08:11 -0300

    Adicionado a validacao que verifica se a data ja passou ao criar um meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -19,14 +21,24 @@ class MeetupController {
     }
 
     const { title, date, description, location, banner_id } = req.body;
+
+    const hourStart = startOfHour(parseISO(date));
+
+    /**
+     * Check for past dates
+     */
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
     /*
-    Não deve ser possível cadastrar meetups com datas que já passaram.
     O usuário também deve poder editar todos dados de meetups que ainda não aconteceram e que ele é organizador.
 
     Crie uma rota para listar os meetups que são organizados pelo usuário logado.
 
     O usuário deve poder cancelar meetups organizados por ele e que ainda não aconteceram. O cancelamento deve deletar o meetup da base de dados.
     */
+
     const meetup = Meetup.create({
       title,
       date,
