4b774b65baa068a030537dc3da7b01c013aa9127
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-27 21:24:32 -0300

    chore: configurado o sentry para tratar os erros em producao. Adicionado o Youch para melhorar a legibilidade do erro em dev.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 // Não precisa informar o index, ele vai detectar automaticamente
 import './database';
@@ -9,12 +13,15 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     // Inserir arquivos estáticos como css, html etc
     this.server.use(
@@ -25,16 +32,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
-    /* this.server.use(
-      async(err, req, res, next => {
-        if (process.env.NODE_ENV === 'development') {
-        }
-      })
-    );
-    return res.status(500).json({ error: 'Internal server error' }); */
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://8355f28239744e26988cfa18563a08a9@sentry.io/1764129',
+};
